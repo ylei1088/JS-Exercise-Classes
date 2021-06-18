e19889f91c137ea991aae630608b401126070e06
ylei1088 <ylei1088@gmail.com>
2021-06-17 23:26:04 -0500

    added code for task 2 -6

--- index.js
+++ index.js
@@ -75,7 +75,28 @@ const mary = new Person("Mary", 50);
           + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
   */
 
-class Car {}
+class Car {
+  constructor(model, milesPerGallon) {
+    this.model = model;
+    this.milesPerGallon = milesPerGallon;
+    this.tank = 0;
+    this.odometer = 0;
+  }
+  fill(gallons) {
+    return this.tank + gallons;
+  }
+  drive(distance) {
+    const driveableMiles = this.tank * this.milesPerGallon;
+    if (distance <= driveableMiles) {
+      this.tank = this.odometer + distance;
+      this.tank = this.tank - distance / this.milesPerGallon;
+    } else {
+      this.odometer = distance;
+      this.tank = 0;
+      return `I ran out of fuel at ${this.odometer} miles`;
+    }
+  }
+}
 
 /*
     TASK 3
@@ -89,7 +110,18 @@ class Car {}
           + Speaking should return a phrase `Hello my name is {name}, I am from {location}`.
           + {name} and {location} of course come from the instance's own properties.
   */
-class Lambdasian {}
+class Lambdasian {
+  constructor(attributes) {
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+  }
+  speak() {
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
+}
+
+const nick = new Lambdasian("Mary", 50, "Chicago");
 
 /*
     TASK 4
@@ -105,7 +137,20 @@ class Lambdasian {}
           + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
           + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
   */
-class Instructor {}
+class Instructor extends Lambdasian {
+  constructor(attrs) {
+    super(attrs);
+    this.specialty = attrs.specialty;
+    this.favLanguage = attrs.favLanguage;
+    this.catchPhrase = attrs.catchPhrase;
+  }
+  demo(subject) {
+    return `'Today we are learning about ${subject}'`;
+  }
+  grade(student, subject) {
+    return `'${student.name} receives a perfect score on ${subject}`;
+  }
+}
 /*
     TASK 5
       - Write a Student class extending Lambdasian.
@@ -121,7 +166,23 @@ class Instructor {}
           + `PRAssignment` a method that receives a subject as an argument and returns `student.name has submitted a PR for {subject}`
           + `sprintChallenge` similar to PRAssignment but returns `student.name has begun sprint challenge on {subject}`
   */
-class Student {}
+class Student extends Lambdasian {
+  constructor(attrs) {
+    super(attrs);
+    this.previousBackground = attrs.previousBackground;
+    this.className = attrs.className;
+    this.favSubjects = ["HTML", "CSS", "JS"];
+  }
+  listSubjects(favSubjects) {
+    return `Loving ${favSubjects}`;
+  }
+  PRAssignment(student, subject) {
+    return ` ${student.name} has submitted a PR for ${subject}`;
+  }
+  sprintChallenge() {
+    return `student.name has begun sprint challenge on ${subject}`;
+  }
+}
 
 /*
     TASK 6
@@ -136,7 +197,19 @@ class Student {}
           + `standUp` a method that takes in a slack channel and returns `{name} announces to {channel}, @channel standy times!`
           + `debugsCode` a method that takes in a student object and a subject and returns `{name} debugs {student.name}'s code on {subject}`
   */
-class ProjectManager {}
+class ProjectManager extends Instructor {
+  constructor(attrs) {
+    super(attrs);
+    this.gradClassName = attrs.gradClassName;
+    this.favInstructor = attrs.favInstructor;
+  }
+  standUp(slackChannel) {
+    return `${this.name} announces to ${slackChannel}, @channel standy times!`;
+  }
+  debugsCode(student, subject) {
+    `${this.name} debugs ${student.name}'s code on ${subject}`;
+  }
+}
 /*
     STRETCH PROBLEM (no tests!)
       - Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
